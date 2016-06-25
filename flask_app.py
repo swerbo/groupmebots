6c86a3d0654bdb16652aee5714a597cd67c77f77
SamW <swerbo@gmail.com>
2016-06-25 21:19:22 +0000

    initial commit

---
+++ flask_app.py
@@ -0,0 +1,64 @@
+
+# A very simple Flask Hello World app for you to get started with...
+import requests,re, json, pymysql
+from flask import Flask, request
+
+app = Flask(__name__)
+
+gmaps_key = "AIzaSyBl-d28wIUowTy9h9miWuvH9ao25zyvdD4"
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask! sd;kjfhglkjsdhfgjklhsdflgkjhsdfg '
+
+@app.route('/bot/', methods=['POST', 'GET'])
+def hello():
+    try:
+        request_json=request.data
+        r_json = json.loads(request_json.decode())
+        summoner = r_json["name"]
+        if re.search('@NYCBROS', r_json["text"].upper()):
+            text = "NYC bros have been summoned by " + summoner
+            requests.post("https://api.groupme.com/v3/bots/post", json = {"text":text, "bot_id" :"10770ab7a49e7d081458c97ea6","attachments":[{"type":"mentions", "loci":[[0,1],[1,1],[2,1],[3,len(text)-4]],"user_ids":["5652869","16538128","16538127","5649747"]}]})
+    except:
+        pass
+    #with open("groupmestuff.txt","wb") as fo:
+    #    fo.write(r_json)
+
+    return "nyc bros"
+
+
+@app.route('/bostonbros/', methods = ['GET','POST'])
+def bostonbros():
+    try:
+        request_json=request.data
+        r_json = json.loads(request_json.decode())
+        summoner = r_json["name"]
+        if re.search('@BOSTONBROS', r_json["text"].upper()):
+            text = "Boston Bros have been summoned by " + summoner
+            #requests.post("https://api.groupme.com/v3/bots/post", json = {"text":text, "bot_id" :"9f21172cf73e54b723efc2ffb9","attachments":[{"type":"mentions", "loci":[[0,1],[1,len(text)]],"user_ids":["5652932","5033991"]}]})
+            requests.post("https://api.groupme.com/v3/bots/post", json = {"text":text, "bot_id" :"9f21172cf73e54b723efc2ffb9","attachments":[{"type":"mentions", "loci":[[0,1],[2,len(text)-2]],"user_ids":["5652932","5033991"]}]})
+    except:
+        pass
+    return "boston bros"
+
+@app.route('/mapsbot/', methods = ['GET','POST'])
+def mapsbot():
+    request_json=request.data
+    r_json = json.loads(request_json.decode())
+    if re.search('@MAPSBOT', r_json["text"].upper()):
+        #get address
+        #encode address
+        formatted_address="655+Grand+Street+Brooklyn,+NY"
+        r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + formatted_address + "&key=AIzaSyBl-d28wIUowTy9h9miWuvH9ao25zyvdD4",)
+        r_json = r.json()
+        lat = str(r_json["results"][0]["geometry"]["location"]["lat"])
+        lng = str(r_json["results"][0]["geometry"]["location"]["lng"])
+        requests.post("https://api.groupme.com/v3/bots/post", json = {"text":lat, "bot_id" :"f6e459ec15721a22ac93598ffd"})
+    return "maps bros"
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
