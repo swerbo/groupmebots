502dbb028ecfcb3703e3c2759b41aca519523c75
jake <jpresky@gmail.com>
2016-07-10 23:38:29 +0000

    Almost totally functioning. We still need to add mentions and have this run when assigned to a schedule. I am not sure how to do it Sam might have a better idea

--- birthdayBot.py
+++ birthdayBot.py
@@ -1,39 +1,46 @@
 import requests
 import UsefulStrings
 from flask import Flask, request
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
-Base= declarative_base()
-
-#Create SQLAlchemy engine. From their website, a typical database url is:: dialect+driver://username:password@host:port/database
-# mysql-python
-engine = create_engine('mysql+mysqldb://swerbo:'+UsefulStrings.sqlpassword+'@swerbo.mysql.pythonanywhere-services.com')
-
-# If ABOVE DOESNT WORK TRY:
-#MySQL-connector-python
-#engine = create_engine('mysql+mysqlconnector://scott:tiger@localhost/foo')
-
-class Bro(Base):
-    __tablename__ = 'bros'
-
-    id=Column(Integer, primary_key=True)
-    name= Column(String)
-    birthday=Column(String)
-    groupme_id=Column(Integer)
-    address=Column(String)
-
-    def __repr__(self):
-        return "<Bro(name'%s', birthday='%s', groupme_id='%d', address='%s')>" % (self.name, self.birthday, self.groupme_id, self.address)
 
 app = Flask(__name__)
+#Connect to the brobook
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://swerbo:'+UsefulStrings.sqlpassword+'@swerbo.mysql.pythonanywhere-services.com/swerbo$brobook'
+#db is an instance of SQLAlchemy
+db = SQLAlchemy(app)
 
+#turns the table bros into a python class
+class bros(db.Model):
+
+
+    id=db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String)
+    birthday=db.Column(db.String)
+    groupme_id=db.Column(db.Integer)
+    address=db.Column(db.String)
+
+    #toStringMethod
+    def __repr__(self):
+        return "<Bro(name='%s', birthday='%s', groupme_id='%d', address='%s')>" % (self.name, self.birthday, self.groupme_id, self.address)
+    #Constructor
+    def __init__(self, name, birthday, groupme_id, address):
+        self.name=name
+        self.birthday=birthday
+        self.groupme_id=groupme_id
+        self.address=address
+
+#puts all bros into a list of bros
+allbros=bros.query.all()
+#Creates a list of the birthday bros
+birthdaybros=[bros for bros in allbros if (bros.birthday.day == datetime.datetime.now().day and bros.birthday.month == datetime.datetime.now().month)]
 
 @app.route('/birthdaybot/', methods = ['GET','POST'])
 def birthdaybot():
-    text=""
-    requests.post("https://api.groupme.com/v3/bots/post", json = {"text":"Happy Birthday to Beautiful BRONAME!", "bot_id" : UsefulStrings.MAPS_BOT_ID, "attachments":[{"type":"mentions", "loci":[[0,1],[2,len(text)-2]],"user_ids": UsefulStrings.BOSTON_BROS}]})
+    for i in range(0,len(birthdaybros)):
+        requests.post("https://api.groupme.com/v3/bots/post", json = {"text":"Happy Birthday to Beautiful "+birthdaybros[i].name+"!", "bot_id" : UsefulStrings.MAPS_BOT_ID}) #I have to figure out mentions
+        #requests.post("https://api.groupme.com/v3/bots/post", json = {"text":"Happy Birthday to Beautiful "+birthdaybros[i].name+"!", "bot_id" : UsefulStrings.MAPS_BOT_ID, "attachments":[{"type":"mentions", "loci":[[0,1]],"user_ids": +birthdaybros[i].groupme_id}]})
     return "Birthday Brah"
 
 
